e45a868a4a0486e3d39241c6fdceead277fd31f4
Tim Danner <tim.danner@solarwinds.com>
2015-01-05 15:58:28 -0600

    Initial commit.

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'OrionSDK' # arbitrary name for the package, used in messages
+$installerType = 'msi' #only one of these: exe, msi, msu
+$url = 'http://downloads.solarwinds.com/solarwinds/Release/Orion/OrionSDK-v1.10.zip' # download url
+$silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\OrionSDK.msi" -validExitCodes $validExitCodes
